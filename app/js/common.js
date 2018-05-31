52f1b8d2f462d670b827c26e97ab73636ebbe1f4
Oleg Gorbatiuk <newgorbole@gmail.com>
2018-05-31 21:07:27 +0200

    31_05_18 backUp

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+$(document).ready(function(){
+		$(".design-card").hover(function(){
+				$(this).children("div:first").removeClass("design-card__content_picture_dev");
+				$(this).children("div:first").addClass("design-card__content_picture_bottle");
+				}, function(){
+				$(this).children("div:first").removeClass("design-card__content_picture_bottle");
+				$(this).children("div:first").addClass("design-card__content_picture_dev");
+				});
+	});
 
-	// Custom JS
 
-});
+// $(document).ready(function(){
+// 	$(this).removeClass("design-card__content_picture_dev");
+// 	$(this).addClass("design-card__content_picture_bottle");
+// });
